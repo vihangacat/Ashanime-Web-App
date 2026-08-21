1e3f6eefdd2c7d95eedc2dc65947dfade142b4ab
vihangacat <vihangacat@gmail.com>
2026-08-21 14:53:00 +0530

    Add Firebase configuration file

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBQzgfhRC9DOarHyJ1Vuc2g59hRns7SjFU",
+  authDomain: "anime-web-8b614.firebaseapp.com",
+  projectId: "anime-web-8b614",
+  storageBucket: "anime-web-8b614.firebasestorage.app",
+  messagingSenderId: "484246752219",
+  appId: "1:484246752219:web:caa1a329a9e7cc7bad75fe",
+  measurementId: "G-PL8YWVYP4W"
+};
